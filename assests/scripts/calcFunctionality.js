080bff265a368704a45029b5de24d971b63e4df9
Revanth Voduru <revanth.voduru@spglobal.com>
2021-11-24 23:40:02 +0530

    Reverted unwanted changes

---
+++ assests/scripts/calcFunctionality.js
@@ -0,0 +1,10 @@
+let currentResult;
+let currentStr;
+
+const defaultResult = 0;
+
+currentResult = Number(defaultResult + 10);
+
+currentStr = "Result of (" + currentResult + '10)'
+
+outputResult(currentResult,currentStr);
